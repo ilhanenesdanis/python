6b081f897bc4706c6f9a88e78e709d0e06656ecb
ilhan enes daniş <danisenes3@gmail.com>
2023-03-28 15:51:10 +0300

    first commit

---
+++ basic-math.py
@@ -0,0 +1,19 @@
+#Math Operators +,-,*,/,**,%,//
+print('toplama operatörü')
+print(3.0+4)#toplama operatörü
+
+print('çarpma operatörü')
+print(3*3)#çarpma operatörü
+print(3.0*3)
+print(3+5*2)#13
+print('bölme operatörü')
+print(3+5*6/2)#bölme operatörü
+print('üs alma operatörü')
+print(2**3)
+print(3**2)
+print(25**0.5)
+print("mod alma operatörü")
+print(10%4)
+print("tam bölme ")
+print(10//3)
+print(93//9)
---
+++ numbers.py
@@ -0,0 +1,5 @@
+print(2+2)
+
+print(type(2)) #<class 'int'>
+print(type(2.5)) #<class 'float'>
+print(2+2.0)
