83ec1868abea88e88b676a1c84c5bbd63d91856b
ilhan enes daniş <danisenes3@gmail.com>
2023-03-28 16:04:47 +0300

    variables,data-types

---
+++ data-types.py
@@ -0,0 +1,20 @@
+#int
+#float
+#string
+#bool
+
+
+x=10
+y=20
+
+toplam=x+y
+print(toplam)
+
+#kullanıcıdan değer almak için input fonksiyonu kullanılır
+
+
+a=int(input("a değerini giriniz "))
+b=int(input("b değerini giriniz "))
+
+kullaniciToplam=a+b
+print(kullaniciToplam)
---
+++ variables.py
@@ -0,0 +1,21 @@
+print(5000+(5000*0.18))
+print(6000+(6000*0.18))
+
+
+urunA=5000
+urunB=6000
+kdv=0.18
+
+print(urunA+(urunA*kdv))
+print(urunB+(urunB*kdv))
+
+#değişken tanımlama kuralları
+
+#sayı ile başlayamaz
+#boşluk içeremez
+#türkçe karatker kullanılmamalıdır
+#büyük küçük harf duyarlılığı vardır
+
+#tek satırda değişken tanımı yapılabilir
+a,b,c=10,20,30
+print(a,b,c)
